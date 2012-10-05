91fddc915ba2076a7f4a4cb0d5dbb83f6327505a
Mickael Kerbrat <stolati@gmail.com>
2012-10-06 00:17:33 +0200

    work year !!!

---
+++ main.py
@@ -0,0 +1,255 @@
+#!/usr/bin/env python3
+
+from itertools import *
+from functools import reduce
+
+
+
+class CellState:
+  UNKNOWN = -1
+  EMPTY = 0
+  FULL = 1
+
+
+class Cell:
+  """Represent a single case of an array
+     have 3 states : empty, really empty, full"""
+
+
+  def __init__(self, state = CellState.UNKNOWN):
+    self.state = state
+
+  def setState(self, state): self.state = state
+  def getState(self, state): return self.state
+
+  def __str__(self): return {CellState.UNKNOWN:'.', CellState.EMPTY:'_', CellState.FULL:'X'}[self.state]
+  def __repr__(self): return str(self)
+
+  @staticmethod
+  def areCompatible(s1, s2):
+    if s1.state == s2.state: return True
+    if s1.state == CellState.UNKNOWN: return True
+    if s2.state == CellState.UNKNOWN: return True
+    return False
+
+  @staticmethod
+  def getCommon(s1, s2):
+    if not Cell.areCompatible(s1, s2): return CellUnknown
+    return s1
+
+  def apply_on_me(self, c):
+    if self.state == CellState.UNKNOWN:
+      self.state = c.state
+      return True
+    return False
+
+
+#static values cells
+class StaticValueCell(Cell):
+  def setState(self, state): pass
+
+CellUnknown = StaticValueCell(CellState.UNKNOWN)
+CellEmpty = StaticValueCell(CellState.EMPTY)
+CellFull = StaticValueCell(CellState.FULL)
+
+
+
+
+
+class LineCol:
+  """
+  Represent a line or a column
+  Have a size, can give all solution in it
+  """
+
+  def __init__(self, elements, numbers):
+    """
+    Take a list of Cell
+    Thoses case can be used by others classes
+    """
+    self.size = len(elements)
+    self.content = list(elements)
+    self.numbers = list(numbers)
+    self.isFull = False
+
+  def test_next(self):
+    """Test for each possibilty of the LineCol
+    then, if some elements are the same for some,
+    change the inside CellState of thoses, making them static
+    return true if at least one cell have changed
+    """
+    if self.isFull : return False
+
+    #print('test_next', self.size, self.content)
+
+    validElements = self.getListValidElements()
+    #print(validElements)
+    if not validElements : return False
+
+    validWithMe = list(filter(self.isGoodWithMe, validElements))
+    #print("validWithMe : ", validWithMe)
+    if not validWithMe : return False
+
+    commons = list(map(self.compareCells, *validWithMe))
+    #print("commons:", commons)
+
+    changed = False
+    self.isFull = True
+    for me, res in zip(self.content, commons):
+      if me.state != CellState.UNKNOWN:
+        assert me.state == res.state
+        continue
+      if res.state == CellState.UNKNOWN:
+        self.isFull = False
+        continue
+      me.state = res.state
+      changed = True
+
+    return changed
+
+
+  def compareCells(self, *cells): return reduce(Cell.getCommon, cells)
+
+
+  def isGoodWithMe(self, state):
+    assert len(state) == self.size
+    return all(map(Cell.areCompatible, state, self.content))
+
+
+
+  def getListValidElements(self):
+    """Return a list of valid line col"""
+    return list(chain(self.genFull(self.size, self.numbers), self.genEmpty(self.size, self.numbers)))
+
+
+  def genEmpty(self, left, numbers):
+    if not numbers: return [repeat(CellEmpty, left)] #no number left, fill all
+    if left < 1:
+      if not numbers: return [[]] #no elements left, return empty 
+      if numbers : return []
+
+    res = []
+    for i in range(1, left):
+      newState = list(repeat(CellEmpty, i))
+      #print('genEmpty', left, numbers, 'loop for', i, '=>', newState)
+
+      for subState in self.genFull(left - i, numbers):
+        #print('genEmpty', left, numbers, '  have subState', list(subState))
+        res.append( list(chain(list(newState), list(subState)))  )
+
+    #print('genEmpty', left, numbers, '=>', res)
+    return res
+
+
+  def genFull(self, left, numbers):
+    if left < 1 or not numbers:
+      #print('genFull', left, numbers, '=>', [])
+      return []
+
+    next_num = numbers[0]
+    if next_num > left :
+      #print('genFull', left, numbers, '=>', [])
+      return []
+
+    res = []
+    newState = list(repeat(CellFull, next_num))
+    for subState in self.genEmpty(left - next_num, numbers[1:]):
+      res.append( list(chain(list(newState), subState) ))
+
+    #print('genFull', left, numbers, '=>', res)
+    return res
+
+
+
+
+class ArrayField:
+  """Represent the full array, list of lines and collumns"""
+
+  def __init__(self, specs):
+    self.colsSpecs, self.lineSpecs = specs.toList()
+
+    self.field = [[Cell() for c in self.colsSpecs] for l in self.lineSpecs]
+
+    self.lines = [LineCol(line, self.lineSpecs[num]) for num, line in enumerate(self.field)]
+    self.cols = [
+        LineCol([line[i] for line in self.field], self.colsSpecs[i])
+        for i in range(len(self.field[0]))
+    ]
+
+  def loopOne(self):
+    changed = False
+    for c in self.cols:
+      if c.test_next():
+        res = True
+        print(self)
+        input()
+
+    for l in self.lines:
+      if l.test_next():
+        res = True
+        print(self)
+        input()
+
+  def __str__(self):
+    res = ''
+    for l in self.field:
+      for c in l:
+        res += ' ' + str(c)
+      res += "\n"
+    return res
+
+
+class SpecReader:
+  """Read specs in multiples format"""
+  def __init__(self, specs):
+
+
+    self.specs = [
+      [
+        [
+          self._numStr2num(numList)
+          for numList in self._numListStr2list(numListList)]
+        for numListList in self._colineStr2list(colline)
+      ]
+      for colline in self._mainStr2list(specs)
+    ]
+
+  def _mainStr2list(self, s):
+    if not isinstance(s, str): return s
+    return [ss for ss in s.split('\n') if ss]
+
+  def _colineStr2list(self, s):
+    if not isinstance(s, str): return s
+    return [ss for ss in s.split(',') if ss]
+
+  def _numListStr2list(self, s):
+    if not isinstance(s, str): return s
+    return [ss for ss in s.split(' ') if ss]
+
+  def _numStr2num(self, s):
+    if not isinstance(s, str): return s
+    return int(s)
+
+
+  def toList(self): return self.specs
+
+
+
+
+
+
+
+if __name__ == "__main__":
+  import maps
+  m = maps.map006
+
+  af = ArrayField(SpecReader(m))
+
+  while True:
+    af.loopOne()
+  #af.loopOne()
+  #af.loopOne()
+  #print(af)
+
+
+#__EOF__
